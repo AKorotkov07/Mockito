9f22af4702a20cb969d47c3ac1aadd0fa6c306dd
AKorotkov07 <153372700+AKorotkov07@users.noreply.github.com>
2024-02-17 08:43:07 +0300

    Add files via upload

--- src/test/java/LocalizationServiceImplTest.java
+++ src/test/java/LocalizationServiceImplTest.java
@@ -1,11 +1,14 @@
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import ru.netology.entity.Country;
+import ru.netology.i18n.LocalizationService;
 import ru.netology.i18n.LocalizationServiceImpl;
 
 public class LocalizationServiceImplTest {
-    private LocalizationServiceImpl localizationService;
+    private LocalizationService localizationService;
 
     @Before
     public void setUp() {
@@ -14,11 +17,17 @@ public class LocalizationServiceImplTest {
 
     @Test
     public void testLocaleForRussia() {
+        LocalizationService localizationServiceMock = mock(LocalizationService.class);
+        when(localizationServiceMock.locale(Country.RUSSIA)).thenReturn("Добро пожаловать");
+
         assertEquals("Добро пожаловать", localizationService.locale(Country.RUSSIA));
     }
 
     @Test
     public void testLocaleForUSA() {
+        LocalizationService localizationServiceMock = mock(LocalizationService.class);
+        when(localizationServiceMock.locale(Country.USA)).thenReturn("Welcome");
+
         assertEquals("Welcome", localizationService.locale(Country.USA));
     }
 }
